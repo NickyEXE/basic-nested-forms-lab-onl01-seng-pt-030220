b99b967f836aec5c9dde3d1a7cb0d1b7744b60f8
NickyEXE <dovern42@gmail.com>
2020-07-17 13:24:09 -0400

    form created

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    3.times do
+      @recipe.ingredients.build
+    end
   end
 
   def create
+    r = Recipe.create(recipe_params)
+    redirect_to r
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
