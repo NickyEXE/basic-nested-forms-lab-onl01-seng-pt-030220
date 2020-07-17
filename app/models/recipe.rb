d6d7e03e25683f83ccee7c0be3d265717d93b969
NickyEXE <dovern42@gmail.com>
2020-07-17 13:03:47 -0400

    clean commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,3 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients
-  accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'] == "" }
+  has_many :ingredients 
 end
